0fcab9da4af98e66a81254b0d2b60dc271b7c009
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-07-31 17:53:25 -0300

    Modulo de Taxa ou Serviço

---
+++ LocadoraDeVeiculos.Dominio/ModuloTaxaServico/TaxaServico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloTaxaServico
+{
+    public class TaxaServico : EntidadeBase<TaxaServico>
+    {
+        public string Nome { get; set; }
+
+        public int Preco { get; set; }
+
+        public EnumTipoCalculo TipoCalculo { get; set; }
+
+        public TaxaServico(int id, string nome, int preco, EnumTipoCalculo tipoCalculo)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            TipoCalculo = tipoCalculo;
+        }
+
+        public override void AlterarInformacoes(TaxaServico entidade)
+        {
+            Id = entidade.Id;
+            Nome = entidade.Nome;
+            Preco = entidade.Preco;
+            TipoCalculo = entidade.TipoCalculo;
+        }
+
+    }
+}
---
+++ LocadoraDeVeiculos.Infra/Migrations/20230731204145_add-TBTaxaServico.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeVeiculos.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTBTaxaServico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBTaxaServico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Preco = table.Column<int>(type: "int", nullable: false),
+                    TipoCalculo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBTaxaServico", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBTaxaServico");
+        }
+    }
+}
