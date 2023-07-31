42a6c5d166fa372081beaa49526d06e7da15d990
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-07-31 17:53:25 -0300

    Modulo de Taxa ou Serviço

---
+++ LocadoraDeVeiculos.Dominio/ModuloTaxaServico/IValidatorTaxaServico.cs
@@ -0,0 +1,6 @@
+﻿namespace LocadoraDeVeiculos.Dominio.ModuloTaxaServico
+{
+    public interface IValidatorTaxaServico : IValidador<TaxaServico>
+    {
+    }
+}
---
+++ LocadoraDeVeiculos.Infra/ModuloTaxaServico/MapeadorTaxaServico.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloTaxaServico;
+using System;
+
+namespace LocadoraDeVeiculos.Infra.ModuloTaxaServico
+{
+    public class MapeadorTaxaServico : IEntityTypeConfiguration<TaxaServico>
+    {
+        public void Configure(EntityTypeBuilder<TaxaServico> builder)
+        {
+            builder.ToTable("TBTaxaServico");
+            builder.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(p => p.Nome).HasColumnType("varchar(250)").IsRequired();
+            builder.Property(p => p.Preco).IsRequired();
+            builder.Property(p => p.TipoCalculo).IsRequired();
+        }
+    }
+}
