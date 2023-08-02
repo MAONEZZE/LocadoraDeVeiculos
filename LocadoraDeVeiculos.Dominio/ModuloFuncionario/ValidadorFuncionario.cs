f01458dfaaea423ba43f6b09a0dd6886367d49b5
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-08-02 16:08:21 -0300

    Back-End do Módulo Funcionário concluído

---
+++ LocadoraDeVeiculos.Dominio/ModuloFuncionario/ValidadorFuncionario.cs
@@ -0,0 +1,32 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloTaxaServico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloFuncionario
+{
+    public class ValidadorFuncionario : AbstractValidator<Funcionario>, IValidadorFuncionario
+    {
+        public ValidadorFuncionario()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(250)
+                .MinimumLength(3)
+                .NaoPodeCaracteresEspeciais();
+
+            RuleFor(x => x.Salario)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThan(0);
+
+            RuleFor(x => x.DataAdmissao)
+                .NotEmpty()
+                .NotNull()
+                .LessThan(DateTime.Now);
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Infra/ModuloFuncionario/MapeadorFuncionario.cs
@@ -0,0 +1,16 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+
+namespace LocadoraDeVeiculos.Infra.ModuloFuncionario
+{
+    public class MapeadorFuncionario : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            builder.ToTable("TBFuncionario");
+            builder.Property(p => p.Id).IsRequired().ValueGeneratedNever();
+            builder.Property(p => p.Nome).HasColumnType("varchar(250)").IsRequired();
+            builder.Property(p => p.DataAdmissao).IsRequired();
+            builder.Property(p => p.Salario).IsRequired();
+        }
+    }
+}
