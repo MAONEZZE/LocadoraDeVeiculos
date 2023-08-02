56b7a3cd4d50c5b22b0aa2ac92dc6d1c12d79249
Bruno <Bruno@DESKTOP-MCC68NG>
2023-08-02 03:46:56 -0300

    Incluindo Branch Develop

--- LocadoraDeVeiculos.WinApp/ModuloAutomovel/ConfiguracaoToolBoxAutomovel.cs
+++ LocadoraDeVeiculos.WinApp/ModuloAutomovel/ConfiguracaoToolBoxAutomovel.cs
@@ -1,18 +1,17 @@
 ﻿
-namespace LocadoraDeVeiculos.WinApp.ModuloAutomovel
+namespace LocadoraDeVeiculos.WinApp.ModuloAutomovel;
+
+internal class ConfiguracaoToolBoxAutomovel : ConfiguracaoToolboxBase
 {
-    internal class ConfiguracaoToolBoxAutomovel : ConfiguracaoToolboxBase
-    {
-        public override string TipoCadastro => "Automóvel";
+    public override string TipoCadastro => "Automóvel";
 
-        public override string TooltipInserir => "Cadastrar Automóvel";
+    public override string TooltipInserir => "Cadastrar Automóvel";
 
-        public override string TooltipEditar => "Editar Automóvel";
+    public override string TooltipEditar => "Editar Automóvel";
 
-        public override string TooltipExcluir => "Excluir Automóvel";
+    public override string TooltipExcluir => "Excluir Automóvel";
 
-        public override string TooltipVisualizar => "Visualizar Automóvel";
+    public override string TooltipVisualizar => "Visualizar Automóvel";
 
-        public override bool VisualizarHabilitado => true;
-    }
+    public override bool VisualizarHabilitado => true;
 }
