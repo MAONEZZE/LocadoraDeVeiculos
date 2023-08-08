a0827663d1ec1fa38bac18217ea2619fccbc4485
Ruan Sanchez <ruansanchezskt@gmail.com>
2023-08-08 16:40:51 -0300

    atualizações

---
+++ LocadoraDeVeiculos.TestesIntegracao/ModuloCliente/RepositorioClienteTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.TestesIntegracao.ModuloCliente
+{
+    [TestClass]
+    public class RepositorioClienteTest
+    {
+    }
+}
---
+++ LocadoraDeVeiculos.TestesUnitarios/2 - Dominio/ModuloCondutor/CondutorTestesUnitarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.TestesUnitarios._2___Dominio.ModuloCondutor
+{
+    internal class CondutorTestesUnitarios
+    {
+    }
+}
